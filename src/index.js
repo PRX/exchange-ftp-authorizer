5c27b090625ce73b409feb6033e2ac7c387c4428
Chris Kalafarski <chris@farski.com>
2024-01-09 13:30:41 -0500

    Use Prettier defaults

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import { createConnection } from 'mysql2/promise';
-import { SSMClient, GetParametersCommand } from '@aws-sdk/client-ssm';
-import { ConfiguredRetryStrategy } from '@aws-sdk/util-retry';
+import { createConnection } from "mysql2/promise";
+import { SSMClient, GetParametersCommand } from "@aws-sdk/client-ssm";
+import { ConfiguredRetryStrategy } from "@aws-sdk/util-retry";
 
 const ssm = new SSMClient({
-  apiVersion: '2014-11-06',
+  apiVersion: "2014-11-06",
   retryStrategy: new ConfiguredRetryStrategy(6, 1100),
   // TODO Shoud also set connectTimeout=1000 and timeout=2000, but couldn't get
   // it working with SDK v3 using requestHandler
@@ -16,16 +16,16 @@ async function authorize(connectionParams, username, password) {
     return false;
   }
 
-  console.log('Creating MySQL connection');
+  console.log("Creating MySQL connection");
   const connection = await createConnection(connectionParams);
-  console.log('Done creating MySQL connection');
+  console.log("Done creating MySQL connection");
 
-  console.log('Running MySQL query');
+  console.log("Running MySQL query");
   const [rows] = await connection.execute(
     "SELECT name FROM `accounts` WHERE delivery_ftp_user = ? AND delivery_ftp_password = ? AND type = 'StationAccount' AND status = 'open' AND deleted_at is NULL",
     [username, password],
   );
-  console.log('Done running MySQL query');
+  console.log("Done running MySQL query");
   connection.end();
 
   if (Array.isArray(rows) && rows.length) {
@@ -36,14 +36,14 @@ async function authorize(connectionParams, username, password) {
 }
 
 async function initializeParams() {
-  console.log('Initializing SSM params');
+  console.log("Initializing SSM params");
 
   return ssm.send(
     new GetParametersCommand({
       Names: [
-        ENV.DB_NAME_PARAMETER_ARN.split(':parameter')[1],
-        ENV.DB_USERNAME_PARAMETER_ARN.split(':parameter')[1],
-        ENV.DB_PASSWORD_PARAMETER_ARN.split(':parameter')[1],
+        ENV.DB_NAME_PARAMETER_ARN.split(":parameter")[1],
+        ENV.DB_USERNAME_PARAMETER_ARN.split(":parameter")[1],
+        ENV.DB_PASSWORD_PARAMETER_ARN.split(":parameter")[1],
       ],
       WithDecryption: true,
     }),
@@ -53,9 +53,9 @@ async function initializeParams() {
 const getParams = await initializeParams();
 
 export const handler = async (event) => {
-  console.log('Getting SSM parameters');
+  console.log("Getting SSM parameters");
   const params = getParams;
-  console.log('Done getting SSM parameters');
+  console.log("Done getting SSM parameters");
 
   const dbConnectionParams = {
     host: ENV.MYSQL_ENDPOINT,
@@ -68,40 +68,40 @@ export const handler = async (event) => {
     ).Value,
   };
 
-  const bucketName = ENV.S3_BUCKET_ARN.split(':')[5];
+  const bucketName = ENV.S3_BUCKET_ARN.split(":")[5];
 
   const userPolicy = {
     Statement: [
       {
-        Action: ['s3:ListBucket'],
+        Action: ["s3:ListBucket"],
         Condition: {
           StringLike: {
-            's3:prefix': [event.username, `${event.username}/*`],
+            "s3:prefix": [event.username, `${event.username}/*`],
           },
         },
-        Effect: 'Allow',
+        Effect: "Allow",
         Resource: [ENV.S3_BUCKET_ARN],
-        Sid: 'AllowBucketUserPrefixReadOnly',
+        Sid: "AllowBucketUserPrefixReadOnly",
       },
       {
         Action: [
-          's3:GetObject',
-          's3:GetObjectAttributes',
-          's3:GetObjectVersion',
-          's3:GetObjectVersionAttributes',
+          "s3:GetObject",
+          "s3:GetObjectAttributes",
+          "s3:GetObjectVersion",
+          "s3:GetObjectVersionAttributes",
         ],
-        Effect: 'Allow',
+        Effect: "Allow",
         Resource: [`${ENV.S3_BUCKET_ARN}/*`],
-        Sid: 'AllowObjectUserPrefixReadOnly',
+        Sid: "AllowObjectUserPrefixReadOnly",
       },
     ],
-    Version: '2012-10-17',
+    Version: "2012-10-17",
   };
 
   const authorization = {
     Role: ENV.FTP_USER_ACCESS_ROLE,
     Policy: JSON.stringify(userPolicy),
-    HomeDirectoryType: 'LOGICAL',
+    HomeDirectoryType: "LOGICAL",
     HomeDirectoryDetails: JSON.stringify([
       {
         // Exposes the contents of a folder in S3 as the root file
@@ -125,17 +125,17 @@ export const handler = async (event) => {
     );
 
     if (isAuthed) {
-      console.log('Password OK');
+      console.log("Password OK");
       return authorization;
     } else {
-      console.log('Bad password');
+      console.log("Bad password");
       return {};
     }
     // } else if (event.protocol === 'SFTP') {
     // Key-based authentication for SFTP
   } else {
     // Invalid authentication; do not return any policy
-    console.log('Bad login');
+    console.log("Bad login");
     return {};
   }
 };
