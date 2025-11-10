8c68da0e6b6ea29c5042032d6b4ce296707f1c5c
Chris Kalafarski <chris@farski.com>
2025-11-10 16:02:36 -0500

    Include all regions in data usage calculation

--- src/rate_limit.js
+++ src/rate_limit.js
@@ -16,14 +16,15 @@ const requestHandler = new NodeHttpHandler({
   socketTimeout: 500,
 });
 
-const cloudwatch = new CloudWatchClient({
-  retryStrategy,
-  requestHandler,
-});
+const REGIONS = ["us-east-1", "us-west-2"];
 
+// Used for both fetching CloudWatch Metrics data points, and bucket refill frequency
 const PERIOD = 3600;
+// How far back to fetch CloudWatch Metrics data
 const NUM_PERIODS = 24 * 7;
-const REFILL_RATE_BYTES = 150_000_000 * 3; // Bytes accrued per metric period. 120 MB is about 1 hour of broadcast MP2 audio
+// Bytes accrued per metric period.
+const REFILL_RATE_BYTES = 150_000_000 * 3; // 120 MB is about 1 hour of broadcast MP2 audio
+// Maximum allowance
 const BUCKET_MAX_BYTES = 10_000_000_000; // 10 GB
 // When we check if the user has any bytes left, we can only chose to let them
 // login or not; we can't actually cut them off at a certain point. Meaning
@@ -68,64 +69,84 @@ export default async function checkUsage(username) {
   const endTime = new Date(Date.now());
 
   const getMetricDataInput = metricQueryInput(username, startTime, endTime);
-  // console.log(JSON.stringify(getMetricDataInput));
 
-  const metricData = await cloudwatch.send(
-    // @ts-ignore
-    new GetMetricDataCommand(getMetricDataInput),
-  );
-  const results = metricData.MetricDataResults?.[0];
-  // console.log(JSON.stringify(results));
+  // Everyone starts with a bucket that is full at `startTime`. We will replay
+  // the usage and refill for each period to determine how many tokens are
+  // remaining at `endTime`.
+  let remaingTokens = BUCKET_MAX_BYTES;
 
-  const dataPoints = results.Timestamps.map((timestamp, i) => ({
-    timestamp: new Date(timestamp),
-    value: results.Values[i],
-  })).sort((a, b) => a.timestamp.valueOf() - b.timestamp.valueOf());
-  // console.log(JSON.stringify(dataPoints));
+  // Check usage in all regions; some stations end up making connections to
+  // both, and we want to consider their total usage.
+  for (const region of REGIONS) {
+    const cloudwatch = new CloudWatchClient({
+      region: region,
+      retryStrategy,
+      requestHandler,
+    });
 
-  const allApproximateTimestamps = [];
-  for (
-    let t = new Date(startTime);
-    t < endTime;
-    t = new Date(t.getTime() + PERIOD * 1000)
-  ) {
-    allApproximateTimestamps.push(new Date(t));
+    const metricData = await cloudwatch.send(
+      // @ts-ignore
+      new GetMetricDataCommand(getMetricDataInput),
+    );
+    // results will look like:
+    // { Timestamps: [2025/01/01 05 AM, 2025/01/01 10 AM, 2025/01/01 11 AM], Values: [0, 5, 0] }
+    const results = metricData.MetricDataResults?.[0];
+
+    // Convert the results in a hash like:
+    // [{ timestamp: 2025/01/01 05 AM, value: 0 }, { timestamp: 2025/01/01 10 AM, value: 5 }]
+    const dataPoints = results.Timestamps.map((timestamp, i) => ({
+      timestamp: new Date(timestamp),
+      value: results.Values[i],
+    })).sort((a, b) => a.timestamp.valueOf() - b.timestamp.valueOf());
+
+    // Create an array of dates for all the periods that exist between
+    // startTime and endTime
+    const allApproximateTimestamps = [];
+    for (
+      let t = new Date(startTime);
+      t < endTime;
+      t = new Date(t.getTime() + PERIOD * 1000)
+    ) {
+      allApproximateTimestamps.push(new Date(t));
+    }
+
+    // Build another hash, where any periods that were missing from the results
+    // are included with a value of 0.
+    // We do this so that we refill for all periods in the range, not just
+    // periods that had a data point in CloudWatch Metrics
+    const filledDataPoints = allApproximateTimestamps.map((aprox) => {
+      const match = dataPoints.find(
+        (p) =>
+          Math.abs(p.timestamp.valueOf() - aprox.valueOf()) <
+          (PERIOD * 1000) / 2, // close enough within 1 period
+      );
+      return {
+        timestamp: aprox,
+        value: match ? match.value : 0,
+      };
+    });
+
+    // For each period, refill and reduce for usage.
+    const tokens = filledDataPoints.reduce((acc, dataPoint) => {
+      const previousTokens = acc;
+
+      // Refill, up to the max
+      const afterRefill = Math.min(
+        BUCKET_MAX_BYTES,
+        previousTokens + REFILL_RATE_BYTES,
+      );
+
+      // Subtract usage for this period, with a min of 0
+      return Math.max(0, afterRefill - dataPoint.value);
+    }, remaingTokens);
+
+    // Update the running total to reflect all regions
+    remaingTokens = tokens;
   }
-  // console.log(JSON.stringify(allApproximateTimestamps));
 
-  const filledDataPoints = allApproximateTimestamps.map((aprox) => {
-    const match = dataPoints.find(
-      (p) =>
-        Math.abs(p.timestamp.valueOf() - aprox.valueOf()) < (PERIOD * 1000) / 2, // close enough within 1 period
-    );
-    return {
-      timestamp: aprox,
-      value: match ? match.value : 0,
-    };
-  });
-  // console.log(JSON.stringify(filledDataPoints));
-
-  const tokens = filledDataPoints.reduce((acc, dataPoint) => {
-    const previousTokens = acc;
-    const purse = Math.min(
-      BUCKET_MAX_BYTES,
-      previousTokens + REFILL_RATE_BYTES,
-    );
-    const spent = dataPoint.value;
-    const finalTokens = Math.max(0, purse - spent);
-
-    // console.log(
-    //   `incoming total: ${previousTokens}, spent: ${spent}, refill: ${REFILL_RATE_BYTES}, final: ${finalTokens}`,
-    // );
-
-    // return Math.max(
-    //   0,
-    //   Math.min(BUCKET_MAX_BYTES, acc - dataPoint.value + REFILL_RATE_BYTES),
-    // );
-    return finalTokens;
-  }, BUCKET_MAX_BYTES);
-
-  const isRateLimited = tokens < FUDGE_BYTES;
+  // If the station has less than the fudge factor remaining, we consider them
+  // to be over the limit and should be rate limited
+  const isRateLimited = remaingTokens < FUDGE_BYTES;
 
   return isRateLimited;
 }
